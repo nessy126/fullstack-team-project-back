1ca9ba82edf535397fa2ae249afd56616a504b9b
ADRIAN TMOCALIUC <adriantmocaliuc@gmail.com>
2022-07-19 10:53:24 +0300

    add sendGrind

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const sgMail = require('@sendgrid/mail');
 
 require('dotenv').config();
+
+const { SENDGRID_API_kEY } = process.env;
+
+sgMail.setApiKey(SENDGRID_API_kEY);
+
+const mail = {
+  to: 'ripoxo3705@teasya.com',
+  from: 'ieroglifr.per@gmail.com',
+  subject: 'Email confirm',
+  html: '<p>Email confirm</p>',
+};
 
 const app = express();
 
