5dcd4ea871d8c36e73ee4e7589d4ac8a560fd6ff
ADRIAN TMOCALIUC <adriantmocaliuc@gmail.com>
2022-07-23 19:46:28 +0300

    change PORT

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const mongoose = require('mongoose');
 
 const app = require('./app');
 
-const { DB_HOST, PORT = 3002 } = process.env;
+const { DB_HOST, PORT = 8000 } = process.env;
 const SUCCESS_MESSAGE = `Database connection successful`;
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(8000);
+    app.listen(PORT);
     console.log(SUCCESS_MESSAGE);
+    console.log('PROT: ', PORT);
   })
   .catch(err => {
     console.log(err.message);
