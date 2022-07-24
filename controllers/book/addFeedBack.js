81eefd4cce6faf228766d89b93590722344caf1e
Anastasiia <65896682+nessy126@users.noreply.github.com>
2022-07-24 17:13:52 +0200

    add func addFeedBack from Artem

--- controllers/book/addFeedBack.js
+++ controllers/book/addFeedBack.js
@@ -1,5 +1,20 @@
+const { Book } = require('../../models/book');
+
 const addFeedBack = async (req, res) => {
   const { bookId } = req.params;
+  const bookReview = await Book.findByIdAndUpdate(
+    bookId,
+    {
+      feedback: req.body,
+    },
+    {
+      new: true,
+    }
+  );
+
+  res.json({
+    bookReview,
+  });
 };
 
 module.exports = addFeedBack;
