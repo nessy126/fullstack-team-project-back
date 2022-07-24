d6d0d49cb1963175ef740762748ffb8e23eed458
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2022-07-24 14:25:49 +0300

    addReview

--- controllers/book/addFeedBack.js
+++ controllers/book/addFeedBack.js
@@ -1,8 +1,22 @@
-const addFeedBack = async (req, res) => {
-  const {bookId} = req.params;
-  
-  
-  console.log("addFeedBack");
-}
+const { Book } = require('../../models/book');
 
-module.exports = addFeedBack
+const addFeedBack = async (req, res) => {
+  const { bookId } = req.params;
+  const { rating, comment } = req.body;
+
+  const bookReview = await Book.findByIdAndUpdate(
+    bookId,
+    {
+      feedback: req.body,
+    },
+    {
+      new: true,
+    }
+  );
+
+  res.json({
+    bookReview,
+  });
+};
+
+module.exports = addFeedBack;
