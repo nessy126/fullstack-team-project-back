ff681ac5eaf2ea0eb8d5540c06ccd8916d452c3f
Artem Faust <91615835+IronHedgehog@users.noreply.github.com>
2022-07-24 14:26:33 +0300

    fix

--- controllers/book/addFeedBack.js
+++ controllers/book/addFeedBack.js
@@ -2,7 +2,6 @@ const { Book } = require('../../models/book');
 
 const addFeedBack = async (req, res) => {
   const { bookId } = req.params;
-  const { rating, comment } = req.body;
 
   const bookReview = await Book.findByIdAndUpdate(
     bookId,
